472eda157f7f8bde6dace48aefc4f24d032d37ec
manish <manishchoudhary55@gmail.com>
2016-08-06 00:02:05 +0530

    Added 404 handling

--- routes/routes.js
+++ routes/routes.js
@@ -19,8 +19,16 @@ router.post('/api/micr', ifsc.findByMicrCode);
 router.post('/api/bank', ifsc.listByBankName);
 router.post('/api/branch', ifsc.findByBranch);
 
-//v1 apis
+// v1 apis
 router.get('/api/v1/ifsc/:ifscCode', ifsc.findByIfscCode_v1);
 router.get('/api/v1/micr/:micrCode', ifsc.findByMicrCode_v1);
 
+//Handle invalid routes
+router.get('*', function(req, res) {
+	res.status(404) // HTTP status 404: NotFound
+	.send({
+		"status" : "failed",
+		"message" : "Invalid route"
+	});
+});
 module.exports = router;
