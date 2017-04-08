7c573239c46fc1c8443d4668b5d4ce8ee42f6477
manish <manish.choudhary@livquik.com>
2017-04-08 20:21:57 +0530

    Added inital java changes

---
+++ src/main/java/in/co/techm/BankApIsApplication.java
@@ -0,0 +1,12 @@
+package in.co.techm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApIsApplication.class, args);
+	}
+}
---
+++ src/test/java/in/co/techm/BankApIsApplicationTests.java
@@ -0,0 +1,16 @@
+package in.co.techm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankApIsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
