d6a1a434974c65f457956bc96fe47d983e59f9e1
manish <manish.choudhary@livquik.com>
2017-05-03 02:28:13 +0530

    dummy endpoint testing

--- src/main/java/in/co/techm/controller/RestContoller.java
+++ src/main/java/in/co/techm/controller/RestContoller.java
@@ -1,9 +1,10 @@
 package in.co.techm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import in.co.techm.model.Bank;
 import in.co.techm.service.BankService;
 
 @RestController
@@ -11,4 +12,10 @@ public class RestContoller {
 	
 	@Autowired
 	BankService mBankService;
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 }
