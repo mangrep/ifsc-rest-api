ea9022161b56c9e04480eabed88d94ae53f28235
mangrep <manishchoudhary55@gmail.com>
2017-06-06 21:46:37 +0530

    Added corse header

--- src/main/java/in/co/techm/WebConfig.java
+++ src/main/java/in/co/techm/WebConfig.java
@@ -2,13 +2,10 @@ package in.co.techm;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.List;
 
 /**
  * Created by livquik on 30/05/17.
@@ -30,8 +27,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     }
 
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new MappingJackson2HttpMessageConverter());
-        super.configureMessageConverters(converters);
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
     }
 }
